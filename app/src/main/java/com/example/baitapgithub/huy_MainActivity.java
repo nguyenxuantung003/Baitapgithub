f76d0e894ebb6a988fdc363f4e167c5f750f3e11
NgHuy1903 <huy781319@gmail.com>
2024-03-19 17:34:52 +0700

    huy commit

---
+++ app/src/main/java/com/example/baitapgithub/huy_MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.baitapgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class huy_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_huy_main);
+    }
+}
